2a9a89d214864626fc0615fea6211f31b9af64cd
Sergey Zhdanov <zhsv007@yandex.ru>
2023-02-26 22:07:29 +0400

    nothing

--- src/app.js
+++ src/app.js
@@ -11,16 +11,16 @@ const menu = new ContextMenu('ul');
 
 
 //CREATE MODULES
-const module_1  = new TestModule ('a', 'BURATINO!')
-const module_2  = new BackgroundModule ('a', 'Change color')
-const module_3  = new ClicksModule ('a', 'Count cliks (for 3 sec)')
-const module_4  = new ShapeModule ('a', 'Create a shape')
+const module_1 = new TestModule('a', 'BURATINO!')
+const module_2 = new BackgroundModule('a', 'Change color')
+const module_3 = new ClicksModule('a', 'Count cliks (for 3 sec)')
+const module_4 = new ShapeModule('a', 'Create a shape')
 const modules = [module_1, module_2, module_3, module_4]
 
 // ADD MODULES TO MENU IN HTML
-modules.forEach((module) =>{
-      menu.add(module)
-    }
+modules.forEach((module) => {
+	menu.add(module)
+}
 )
 
 // RIGHT CLICK LOGIC
@@ -28,14 +28,14 @@ menu.open();
 
 //MENU CLICK LOGIC
 menu.el.addEventListener('click', (event) => {
-  if (event.target.nodeName === 'LI') {
-    const clickedText = event.target.outerText;
-    const clickedModule = modules.filter((element) => {
-      return element.text === clickedText;
-    });
+	if (event.target.nodeName === 'LI') {
+		const clickedText = event.target.outerText;
+		const clickedModule = modules.filter((element) => {
+			return element.text === clickedText;
+		});
 
-    clickedModule[0].trigger();
-    menu.close();
-  }
+		clickedModule[0].trigger();
+		menu.close();
+	}
 });
 
--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,8 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
+
 
 export class ShapeModule extends Module {
 
+
 }
+
